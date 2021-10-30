e290581542e03985b46f8bc818423df610972186
dtaylor-us <derektaylor.us@gmail.com>
2021-10-30 01:31:59 -0500

    init project

---
+++ src/main/java/com/dtaylorus/kafkaangularspringboot/KafkaAngularSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.dtaylorus.kafkaangularspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAngularSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaAngularSpringbootApplication.class, args);
+    }
+
+}
